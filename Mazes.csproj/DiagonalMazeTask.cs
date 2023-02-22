822d136610b90f8413c372e05243931d550e6ccb
Elena <foxichenochek@yandex.ru>
2023-02-22 09:40:34 +0400

    Init Project
    Need to do last 3 mazes

---
+++ Mazes.csproj/DiagonalMazeTask.cs
@@ -0,0 +1,21 @@
+﻿namespace Mazes
+{
+	public static class DiagonalMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			int widthStep = 1;
+			int heihthStep = 1;
+			if (width > height) widthStep =(int)System.Math.Round((double)width / height);
+			else heihthStep = (int)System.Math.Round((double)height / width);
+		}
+
+		public static void MoveOneStep(Robot robot,Direction direction, int step)
+        {
+            for (int i = 0; i < step; i++)
+            {
+				robot.MoveTo(direction);
+            }
+        }
+	}
+}
---
+++ Mazes.csproj/SnakeMazeTask.cs
@@ -0,0 +1,37 @@
+﻿namespace Mazes
+{
+	public static class SnakeMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			height -= 2;
+			while(height > 1)
+            {
+				MoveOneCicle(robot, width, height);
+				height -= 3;
+				if (height > 0)
+					MoveDown(robot, ref height);
+			}
+		}
+
+		public static void MoveOneCicle(Robot robot, int width, int height)
+        {
+			MoveXDirection(robot, width, Direction.Right);
+			MoveDown(robot, ref height);
+			MoveXDirection(robot, width, Direction.Left);
+        }
+
+		public static void MoveXDirection(Robot robot, int width, Direction direction)
+        {
+			for (int i = 0; i < width - 3; i++)
+				robot.MoveTo(direction);
+		}
+		
+		public static void MoveDown(Robot robot, ref int height)
+        {
+            for (int i = 0; i < 2; i++)
+				robot.MoveTo(Direction.Down);
+			height--;
+        }
+	}
+}
